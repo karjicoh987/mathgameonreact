c1dcbfef5891a907085136f6d974bde1295ae8ad
unknown <krutoy-paren123@yandex.ru>
2020-01-19 18:05:11 +0500

    rewrite on function comonent LvlPanel

--- src/components/LvlPanel/LvlPanel.js
+++ src/components/LvlPanel/LvlPanel.js
@@ -3,18 +3,16 @@ import LvlData from './LvlData'
 import LvlCard from './LvlCard'
 import './lvlstyle.css'
 
-class LvlPanel extends Component{
-    render(){
-        const LvlCards = LvlData.map(
-            (card,i) => <LvlCard key = {i} {...card}/>
-        )
-        return(
-            <div className = "Flex-box">
-                {LvlCards}
-            </div>
-            
-        )
-    }
+const LvlPanel = () => {
+    const LvlCards = LvlData.map(
+        (card,i) => <LvlCard key = {i} {...card}/>
+    )
+    return(
+        <div className = "Flex-box">
+            {LvlCards}
+        </div>
+        
+    )
 }
 
 export default LvlPanel
